62bead59f95075b392cffbc4ca91c2d8123e2572
Daniel Basilio <daniel.basilio@tophat.com>
2021-05-13 18:45:26 -0400

    feat: add config properties and candidate logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-import { CommandContext, Configuration, Plugin, Project } from '@yarnpkg/core'
-import { Command, Usage } from 'clipanion'
+import {
+    CommandContext,
+    Configuration,
+    Descriptor,
+    Manifest,
+    Plugin,
+    Project,
+    miscUtils,
+} from '@yarnpkg/core'
+import { npath, ppath } from '@yarnpkg/fslib'
+import { Command, Option, Usage } from 'clipanion'
+
+type Config = {
+    packagePrefix?: string
+    allowMajor?: boolean
+    allowMinor?: boolean
+    allowPatch?: boolean
+    maxPackagesUpdated?: number
+}
+
+const defaultConfig: Config = {
+    allowMajor: false,
+    allowMinor: true,
+    allowPatch: true,
+    maxPackagesUpdated: 1,
+}
 
 class SemverUpCommand extends Command<CommandContext> {
     static paths = [['semver', 'up']]
@@ -9,6 +33,8 @@ class SemverUpCommand extends Command<CommandContext> {
         details: '',
         examples: [],
     })
+
+    configFile?: string = Option.String('--config', { required: false })
 
     async execute(): Promise<number> {
         try {
@@ -23,12 +49,78 @@ class SemverUpCommand extends Command<CommandContext> {
 
             await project.restoreInstallState()
 
+            const config = await this.parseConfigFile()
+            await this.getUpdateCandidates(
+                config,
+                project.topLevelWorkspace.manifest,
+            )
+
             this.context.stdout.write('Done\n')
             return 0
         } catch (err) {
             this.context.stderr.write(`${String(err)}\n`)
             return 1
         }
+    }
+
+    async parseConfigFile(): Promise<Config> {
+        const configFile = this.configFile ? this.configFile : '.semver-up.json'
+
+        const configPPath = ppath.resolve(
+            ppath.cwd(),
+            npath.toPortablePath(configFile),
+        )
+
+        let packageConfig: Config = {}
+        try {
+            packageConfig = miscUtils.dynamicRequireNoCache(configPPath)
+        } catch (e) {
+            this.context.stdout.write(
+                '.semver-up.json file not detected, using default config\n',
+            )
+        }
+
+        const config = {
+            ...defaultConfig,
+            ...packageConfig,
+        }
+
+        if (!config.allowMajor && !config.allowMinor && !config.allowPatch) {
+            throw new Error(
+                'One of [allowMajor, allowMinor, allowPatch] must be set to true',
+            )
+        }
+
+        this.context.stdout.write(
+            `Using config: ${JSON.stringify(config, null, 4)}\n`,
+        )
+        return config
+    }
+
+    async getUpdateCandidates(
+        config: Config,
+        manifest: Manifest,
+    ): Promise<Descriptor[]> {
+        const candidates: Descriptor[] = []
+        const possiblePackages = [
+            ...manifest.dependencies.values(),
+            ...manifest.devDependencies.values(),
+        ]
+        if (config.packagePrefix) {
+            for (const dependency of possiblePackages) {
+                if (dependency.name.startsWith(config.packagePrefix))
+                    candidates.push(dependency)
+            }
+        } else {
+            candidates.push(...possiblePackages)
+        }
+
+        this.context.stdout.write(
+            `Candidate packages for upgrade are:\n${candidates
+                .map(candidate => candidate.name)
+                .join('\n')}\n`,
+        )
+        return candidates
     }
 }
 
